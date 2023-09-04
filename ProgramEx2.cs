dfce2d05430e9abd3f96f34e1d0b27e65664bcd4
gabriel35423 <137359307+gabriel35423@users.noreply.github.com>
2023-09-04 19:12:57 -0300

    Add files via upload

---
+++ ProgramEx2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPOOex1
+{
+    internal class Quadrado
+    {
+
+        private double l;
+        private double a;
+    
+
+        public void setl(double q)
+        {
+            l = q;
+
+        }
+        public void seta(double q)
+        {
+            l = q;
+
+        }
+
+        public double getl()
+        {
+            return l;
+        }
+
+        public double geta()
+        {
+            return a;
+        }
+
+        public void calcular()
+        {
+            a = l * l;
+        }
+
+
+    }
+
+}
